e858b25ffad90edc6ce1007cea706ef1992b63f3
Jyotirmay01 <jyotirmay@kgpian.iitkgp.ac.in>
2026-05-07 17:21:45 +0530

    redis related changes

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings:
     PROJECT_NAME = 'Car Price API'
-    API_KEY = os.getenv('API_KEY','demo_key')
+    API_KEY = os.getenv('API_KEY','demo-key')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY','secret')
     JWT_ALGORITHM = 'HS256'
     REDIS_URL = os.getenv('REDIS_URL','redis://localhost:6379')
