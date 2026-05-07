63941066fffb0055a2b857e73acd9a5c89d3a033
Jyotirmay01 <jyotirmay@kgpian.iitkgp.ac.in>
2026-05-07 12:45:02 +0530

    new changes

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -4,6 +4,7 @@ from app.core.security import verify_token
 
 def get_api_key(api_key: str = Header(...)):
     if api_key!=settings.API_KEY:
+        print(f"Expected: {settings.API_KEY} | Received: {api_key}")
         raise HTTPException(status_code=403, detail='Invalid API Key')
     
 def get_current_user(token: str=Header(...)):
